5cedee6a2a2b9e446914c4d4f05080648ec47200
sky <sky990319@gmail.com>
2023-03-10 15:52:30 +0900

    Updata

--- KITTI/downloader.py
+++ KITTI/downloader.py
@@ -18,7 +18,7 @@ URL_BASE = "https://s3.eu-central-1.amazonaws.com/avg-kitti/"
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--kitti_dir', type=str, default= "kitti/")
+    parser.add_argument('--kitti_dir', type=str, default= "/content/kitti/")
     parser.add_argument('--zip_name', type=str, default= "data_odometry_color.zip")
 
     return parser.parse_args(sys.argv[1:])
@@ -29,17 +29,20 @@ def main():
     kitti_dir = args.kitti_dir
     zip_name = args.zip_name
 
-    os.makedirs(kitti_dir, exist_ok=True)
-
+    if not os.path.exists(kitti_dir) : os.makedirs(kitti_dir, exist_ok = True)
+    os.chdir(kitti_dir)
+    
+    print("Current dir {}".format(os.getcwd()))
     url = URL_BASE + zip_name
 
     if os.path.exists(zip_name):
         print("File {} exists. Not re-downloading.".format(zip_name))
     else:
         url = URL_BASE + zip_name
+        
         print("Downloading file {} to folder {}.".format(zip_name, kitti_dir))
         call(['wget', url])
-
+        
         call(['unzip', '-o', zip_name])
 
 
